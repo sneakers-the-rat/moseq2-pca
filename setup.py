8c6d174ba0449a63e38b08e96f86505065b3e735
jmarkow <jmarkow@cns.bu.edu>
2018-05-04 12:35:57 -0400

    Added dask_jobqueue requirement

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     install_requires=['h5py', 'matplotlib', 'scipy>=0.19',
                       'tqdm', 'numpy==1.13.1', 'joblib==0.11',
                       'opencv-python', 'click', 'ruamel.yaml',
-                      'dask[complete]', 'chest', 'seaborn'],
+                      'dask[complete]', 'chest', 'seaborn', 'dask_jobqueue'],
     python_requires='>=3.6',
     entry_points={'console_scripts': ['moseq2-pca = moseq2_pca.cli:cli']}
 )
