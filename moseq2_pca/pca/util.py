2d758642605afb1707a8053cf2e7c2e1bcce4bc2
Jeff Markowitz <jeffrey_markowitz@hms.harvard.edu>
2018-05-07 01:19:41 -0400

    Bugfix

--- moseq2_pca/pca/util.py
+++ moseq2_pca/pca/util.py
@@ -59,7 +59,7 @@ def train_pca_dask(dask_array, clean_params, use_fft, rank,
             u, s, v = lng.svd_compressed(dask_array-mean, rank, 0)
             recon = u[:, :recon_pcs].dot(s[:recon_pcs][None, :])
             recon = recon.dot(v[:recon_pcs, :]) + mean
-            dask_array = da.map_blocks(mask_data, dask_array, mask, recon, dtype=mask_data.dtype)
+            dask_array = da.map_blocks(mask_data, dask_array, mask, recon, dtype=dask_array.dtype)
             # dask_array[~mask] = recon[~mask]
             mean = dask.array.mean(axis=0)
 
