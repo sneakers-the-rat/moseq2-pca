306ef377eb8484d6a2f07f6c2828fe3dfa7f1467
Jeff Markowitz <jeffrey_markowitz@hms.harvard.edu>
2018-05-07 00:13:12 -0400

    Bugfix

--- moseq2_pca/pca/util.py
+++ moseq2_pca/pca/util.py
@@ -41,7 +41,9 @@ def train_pca_dask(dask_array, clean_params, use_fft, rank,
     tmp = da.random.random(dask_array.shape, chunks=dask_array.chunks)
     print(mask.chunks)
     print(dask_array.chunks)
-    dask_array[da.where(~mask)] = tmp[da.where(~mask)]
+    idx = da.where(~mask)
+    for i in idx:
+        dask_array[i] = tmp[i]
 
     nsamples, nfeatures = dask_array.shape
     mean = dask_array.mean(axis=0)
