43e35c6d005524eed0ac10aaa5c419690ca79af1
Jeff Markowitz <jeffrey_markowitz@hms.harvard.edu>
2018-05-30 13:53:51 -0400

    Bump version for scikit-image

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
                       'tqdm', 'numpy==1.13.1', 'joblib==0.11',
                       'opencv-python', 'click', 'ruamel.yaml',
                       'dask[complete]', 'chest', 'seaborn', 'dask_jobqueue',
-                      'scikit-image'],
+                      'scikit-image==0.13'],
     python_requires='>=3.6',
     entry_points={'console_scripts': ['moseq2-pca = moseq2_pca.cli:cli']}
 )
