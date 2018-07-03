34aef77e68a6fbb1db7e966b6be03c66526517fe
Winthrop Gillis <win.gillis@gmail.com>
2018-07-03 20:57:48 +0000

    fixed bug with selecting a None-type tailfilter

--- moseq2_pca/util.py
+++ moseq2_pca/util.py
@@ -141,13 +141,12 @@ def clean_frames(frames, medfilter_space=None, gaussfilter_space=None,
 def select_strel(string='e', size=(10, 10)):
     if string is None or 'none' in string or np.all(np.array(size) == 0):
         strel = None
-    if string[0].lower() == 'e':
+    elif string[0].lower() == 'e':
         strel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, size)
     elif string[0].lower() == 'r':
         strel = cv2.getStructuringElement(cv2.MORPH_RECT, size)
     else:
         strel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, size)
-
     return strel
 
 
