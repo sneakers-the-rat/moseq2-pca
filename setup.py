68911f9c44a3d07026b7592f64dd28e7dca6ca08
jmarkow <jmarkow@cns.bu.edu>
2019-05-13 12:29:05 -0400

    Moved cv2 out of install_requires and now using more flexible numpy version

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 from setuptools import setup, find_packages
 
+
+def install(package):
+    subprocess.call([sys.executable, "-m", "pip", "install", package])
+
+
+try:
+    import cv2
+except ImportError:
+    install('opencv-python')
+
+
 setup(
     name='moseq2-pca',
     author='Jeff Markowitz',
@@ -8,8 +19,8 @@ setup(
     platforms=['mac', 'unix'],
     packages=find_packages(),
     install_requires=['h5py', 'matplotlib', 'scipy>=0.19',
-                      'tqdm', 'numpy==1.14.5', 'joblib==0.13.1',
-                      'opencv-python', 'click', 'ruamel.yaml>=0.15.0',
+                      'tqdm', 'numpy', 'joblib==0.13.1',
+                      'click', 'ruamel.yaml>=0.15.0',
                       'dask[complete]', 'chest', 'seaborn', 'dask_jobqueue>=0.3.0',
                       'scikit-image>=0.14', 'bokeh', 'psutil'],
     python_requires='>=3.6',
