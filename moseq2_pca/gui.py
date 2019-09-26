00a4ebeab1fd4aca1dc580353cc94f06158a116e
Ayman Zeine <a.zeine.96@gmail.com>
2019-09-26 14:31:18 -0400

    Updated GUI

--- moseq2_pca/gui.py
+++ moseq2_pca/gui.py
@@ -14,21 +14,18 @@ import dask.array as da
 import tqdm
 import pathlib
 
+def train_pca_command(input_dir, config_file, output_dir, output_file):
 
-def train_pca_command(input_dir, cluster_type, output_dir, gaussfilter_space,
-              gaussfilter_time, medfilter_space, medfilter_time, missing_data, missing_data_iters,
-              mask_threshold, mask_height_threshold, min_height, max_height, tailfilter_size,
-              tailfilter_shape, use_fft, recon_pcs, rank, output_file, chunk_size,
-              config_file, local_processes, queue, nworkers,
-              cores, processes, memory, wall_time, timeout):
+    with open(config_file, 'r') as f:
+        config_data = yaml.safe_load(f)
 
     dask_cache_path = os.path.join(pathlib.Path.home(), 'moseq2_pca')
     # find directories with .dat files that either have incomplete or no extractions
 
-    if missing_data and use_fft:
+    if config_data['missing_data'] and config_data['use_fft']:
         raise NotImplementedError("FFT and missing data not implemented yet")
 
-    params = locals()
+    params = config_data
     h5s, dicts, yamls = recursive_find_h5s(input_dir)
     timestamp = '{:%Y-%m-%d_%H-%M-%S}'.format(datetime.datetime.now())
 
@@ -38,7 +35,7 @@ def train_pca_command(input_dir, cluster_type, output_dir, gaussfilter_space,
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    save_file = os.path.join(output_dir, output_file)
+    save_file = output_dir+output_file
 
     if os.path.exists('{}.h5'.format(save_file)):
         raise IOError('{}.h5 already exists, delete before recomputing'.format(save_file))
@@ -47,45 +44,45 @@ def train_pca_command(input_dir, cluster_type, output_dir, gaussfilter_space,
     with open(config_store, 'w') as f:
         yaml.dump(params, f, Dumper=yaml.RoundTripDumper)
 
-    tailfilter = select_strel(tailfilter_shape, tailfilter_size)
+    tailfilter = select_strel((config_data['tailfilter_shape'], config_data['tailfilter_size']))
 
     clean_params = {
-        'gaussfilter_space': gaussfilter_space,
-        'gaussfilter_time': gaussfilter_time,
+        'gaussfilter_space': config_data['gaussfilter_space'],
+        'gaussfilter_time': config_data['gaussfilter_time'],
         'tailfilter': tailfilter,
-        'medfilter_time': medfilter_time,
-        'medfilter_space': medfilter_space
+        'medfilter_time': config_data['medfilter_time'],
+        'medfilter_space': config_data['medfilter_space']
     }
 
     # dask.set_options(temporary_directory='/home/jmarkow/dask-tmp')
 
     client, cluster, workers, cache =\
-        initialize_dask(cluster_type=cluster_type,
-                        nworkers=nworkers,
-                        cores=cores,
-                        processes=processes,
-                        local_processes=local_processes,
-                        memory=memory,
-                        wall_time=wall_time,
-                        queue=queue,
-                        timeout=timeout,
+        initialize_dask(cluster_type=config_data['cluster_type'],
+                        nworkers=config_data['nworkers'],
+                        cores=config_data['cores'],
+                        processes=config_data['processes'],
+                        local_processes=config_data['local_processes'],
+                        memory=config_data['memory'],
+                        wall_time=config_data['wall_time'],
+                        queue=config_data['queue'],
+                        timeout=config_data['timeout'],
                         scheduler='distributed',
                         cache_path=dask_cache_path)
 
     dsets = [h5py.File(h5, mode='r')['/frames'] for h5 in h5s]
-    arrays = [da.from_array(dset, chunks=(chunk_size, -1, -1)) for dset in dsets]
+    arrays = [da.from_array(dset, chunks=(config_data['chunk_size'], -1, -1)) for dset in dsets]
     stacked_array = da.concatenate(arrays, axis=0)
-    stacked_array[stacked_array < min_height] = 0
-    stacked_array[stacked_array > max_height] = 0
+    stacked_array[stacked_array < config_data['min_height']] = 0
+    stacked_array[stacked_array > config_data['max_height']] = 0
 
     print('Processing {:d} total frames'.format(stacked_array.shape[0]))
 
-    if missing_data:
+    if config_data['missing_data']:
         mask_dsets = [h5py.File(h5, mode='r')['/frames_mask'] for h5 in h5s]
-        mask_arrays = [da.from_array(dset, chunks=(chunk_size, -1, -1)) for dset in mask_dsets]
+        mask_arrays = [da.from_array(dset, chunks=(config_data['chunk_size'], -1, -1)) for dset in mask_dsets]
         stacked_array_mask = da.concatenate(mask_arrays, axis=0).astype('float32')
-        stacked_array_mask = da.logical_and(stacked_array_mask < mask_threshold,
-                                            stacked_array > mask_height_threshold)
+        stacked_array_mask = da.logical_and(stacked_array_mask < config_data['mask_threshold'],
+                                            stacked_array > config_data['mask_height_threshold'])
         print('Loaded mask...')
         # stacked_array_mask = dask.compute(stacked_array_mask)
     else:
@@ -93,11 +90,11 @@ def train_pca_command(input_dir, cluster_type, output_dir, gaussfilter_space,
 
     output_dict =\
         train_pca_dask(dask_array=stacked_array, mask=stacked_array_mask,
-                       clean_params=clean_params, use_fft=use_fft,
-                       rank=rank, cluster_type=cluster_type, min_height=min_height,
-                       max_height=max_height, client=client,
-                       iters=missing_data_iters, workers=workers, cache=cache,
-                       recon_pcs=recon_pcs)
+                       clean_params=clean_params, use_fft=config_data['use_fft'],
+                       rank=config_data['rank'], cluster_type=config_data['cluster_type'], min_height=config_data['min_height'],
+                       max_height=config_data['max_height'], client=client,
+                       iters=config_data['missing_data_iters'], workers=workers, cache=cache,
+                       recon_pcs=config_data['recon_pcs'])
 
 
     if cluster is not None:
@@ -121,15 +118,20 @@ def train_pca_command(input_dir, cluster_type, output_dir, gaussfilter_space,
         for k, v in output_dict.items():
             f.create_dataset(k, data=v, compression='gzip', dtype='float32')
 
+    config_data['pca_file'] = './'+save_file+'.h5'
+    with open(config_file, 'w') as f:
+        yaml.dump(config_data, f, Dumper=yaml.RoundTripDumper)
+
     return True
 
 
-def apply_pca_command(input_dir, cluster_type, output_dir, output_file, h5_path, h5_mask_path,
-              pca_path, pca_file, chunk_size, fill_gaps, fps, detrend_window,
-              config_file, queue, nworkers, cores, processes, memory, wall_time, timeout):
+def apply_pca_command(input_dir, config_file, output_dir, output_file):
     # find directories with .dat files that either have incomplete or no extractions
     # TODO: additional post-processing, intelligent mapping of metadata to group names, make sure
     # moseq2-model processes these files correctly
+
+    with open(config_file, 'r') as f:
+        config_data = yaml.safe_load(f)
 
     dask_cache_path = os.path.join(pathlib.Path.home(), 'moseq2_pca')
     params = locals()
@@ -139,23 +141,23 @@ def apply_pca_command(input_dir, cluster_type, output_dir, output_file, h5_path,
     h5_timestamp_path = get_timestamp_path(h5s[0])
     h5_metadata_path = get_metadata_path(h5s[0])
 
-    if pca_file is None:
+    if config_data['pca_file'] is None:
         pca_file = os.path.join(output_dir, 'pca.h5')
 
-    if not os.path.exists(pca_file):
-        raise IOError('Could not find PCA components file {}'.format(pca_file))
+    if not os.path.exists(config_data['pca_file']):
+        raise IOError('Could not find PCA components file {}'.format(config_data['pca_file']))
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    save_file = os.path.join(output_dir, output_file)
+    save_file = output_dir+output_file
 
-    print('Loading PCs from {}'.format(pca_file))
-    with h5py.File(pca_file, 'r') as f:
-        pca_components = f[pca_path][...]
+    print('Loading PCs from {}'.format(config_data['pca_file']))
+    with h5py.File(config_data['pca_file'], 'r') as f:
+        pca_components = f[config_data['pca_path']][...]
 
     # get the yaml for pca, check parameters, if we used fft, be sure to turn on here...
-    pca_yaml = '{}.yaml'.format(os.path.splitext(pca_file)[0])
+    pca_yaml = '{}.yaml'.format(os.path.splitext(config_data['pca_file'])[0])
 
     # todo detect missing data and mask parameters, then 0 out, fill in, compute scores...
     if os.path.exists(pca_yaml):
@@ -199,29 +201,29 @@ def apply_pca_command(input_dir, cluster_type, output_dir, output_file, h5_path,
 
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", tqdm.TqdmSynchronisationWarning)
-        if cluster_type == 'nodask':
+        if config_data['cluster_type'] == 'nodask':
             apply_pca_local(pca_components=pca_components, h5s=h5s, yamls=yamls,
                             use_fft=use_fft, clean_params=clean_params,
-                            save_file=save_file, chunk_size=chunk_size,
-                            mask_params=mask_params, fps=fps,
+                            save_file=save_file, chunk_size=config_data['chunk_size'],
+                            mask_params=mask_params, fps=config_data['fps'],
                             missing_data=missing_data)
 
         else:
             client, cluster, workers, cache =\
-             initialize_dask(cluster_type=cluster_type,
-                             nworkers=nworkers,
-                             cores=cores,
-                             processes=processes,
-                             memory=memory,
-                             wall_time=wall_time,
-                             queue=queue,
+             initialize_dask(cluster_type=config_data['cluster_type'],
+                             nworkers=config_data['nworkers'],
+                             cores=config_data['cores'],
+                             processes=config_data['processes'],
+                             memory=config_data['memory'],
+                             wall_time=config_data['wall_time'],
+                             queue=config_data['queue'],
                              scheduler='distributed',
-                             timeout=timeout,
+                             timeout=config_data['timeout'],
                              cache_path=dask_cache_path)
             apply_pca_dask(pca_components=pca_components, h5s=h5s, yamls=yamls,
                            use_fft=use_fft, clean_params=clean_params,
-                           save_file=save_file, chunk_size=chunk_size,
-                           fps=fps, client=client, missing_data=missing_data,
+                           save_file=save_file, chunk_size=config_data['chunk_size'],
+                           fps=config_data['fps'], client=client, missing_data=missing_data,
                            mask_params=mask_params)
 
             if cluster is not None:
@@ -229,12 +231,18 @@ def apply_pca_command(input_dir, cluster_type, output_dir, output_file, h5_path,
                     shutdown_dask(cluster.scheduler)
                 except:
                     pass
+
+    config_data['pca_file_scores'] = './'+save_file+'.h5'
+    with open(config_file, 'w') as f:
+        yaml.dump(config_data, f, Dumper=yaml.RoundTripDumper)
+
     return True
 
-def compute_changepoints_command(input_dir, output_dir, output_file, cluster_type, pca_file_components,
-                         pca_file_scores, pca_path, neighbors, threshold, klags, sigma, dims, fps, h5_path,
-                         h5_mask_path, chunk_size, config_file,
-                         queue, nworkers, cores, processes, memory, wall_time, timeout):
+
+def compute_changepoints_command(input_dir, config_file, output_dir, output_file):
+
+    with open(config_file, 'r') as f:
+        config_data = yaml.safe_load(f)
 
     dask_cache_path = os.path.join(pathlib.Path.home(), 'moseq2_pca')
     params = locals()
@@ -242,26 +250,31 @@ def compute_changepoints_command(input_dir, output_dir, output_file, cluster_typ
 
     h5_timestamp_path = get_timestamp_path(h5s[0])
 
-    if pca_file_components is None:
+    if config_data['pca_file_components'] is None:
         pca_file_components = os.path.join(output_dir, 'pca.h5')
+        config_data['pca_file_components'] = pca_file_components
+        with open(config_file, 'w') as f:
+            yaml.dump(config_data, f, Dumper=yaml.RoundTripDumper)
+    else:
+        pca_file_components = config_data['pca_file_components']
 
-    if pca_file_scores is None:
+    if config_data['pca_file_scores'] is None:
         pca_file_scores = os.path.join(output_dir, 'pca_scores.h5')
 
-    if not os.path.exists(pca_file_components):
-        raise IOError('Could not find PCA components file {}'.format(pca_file_components))
+    if not os.path.exists(config_data['pca_file_components']):
+        raise IOError('Could not find PCA components file {}'.format(config_data['pca_file_components']))
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    save_file = os.path.join(output_dir, output_file)
+    save_file = output_dir+output_file
 
     print('Loading PCs from {}'.format(pca_file_components))
     with h5py.File(pca_file_components, 'r') as f:
-        pca_components = f[pca_path][...]
+        pca_components = f[config_data['pca_path']][...]
 
     # get the yaml for pca, check parameters, if we used fft, be sure to turn on here...
-    pca_yaml = '{}.yaml'.format(os.path.splitext(pca_file_components)[0])
+    pca_yaml = '{}.yaml'.format(os.path.splitext(config_data['pca_file_components'])[0])
 
     # todo detect missing data and mask parameters, then 0 out, fill in, compute scores...
     if os.path.exists(pca_yaml):
@@ -280,33 +293,33 @@ def compute_changepoints_command(input_dir, output_dir, output_file, cluster_typ
                 pca_file_scores = None
                 mask_params = None
 
-            if missing_data and not os.path.exists(pca_file_scores):
+            if missing_data and not os.path.exists(config_data['pca_file_scores']):
                 raise RuntimeError("Need PCA scores to impute missing data, run apply pca first")
 
     changepoint_params = {
-        'k': klags,
-        'sigma': sigma,
-        'peak_height': threshold,
-        'peak_neighbors': neighbors,
-        'rps': dims
+        'k': config_data['klags'],
+        'sigma': config_data['sigma'],
+        'peak_height': config_data['threshold'],
+        'peak_neighbors': config_data['neighbors'],
+        'rps': config_data['dims']
     }
 
     client, cluster, workers, cache =\
-        initialize_dask(cluster_type=cluster_type,
-                        nworkers=nworkers,
-                        cores=cores,
-                        processes=processes,
-                        memory=memory,
-                        wall_time=wall_time,
-                        queue=queue,
+        initialize_dask(cluster_type=config_data['cluster_type'],
+                        nworkers=config_data['nworkers'],
+                        cores=config_data['cores'],
+                        processes=config_data['processes'],
+                        memory=config_data['memory'],
+                        wall_time=config_data['wall_time'],
+                        queue=config_data['queue'],
                         scheduler='distributed',
-                        timeout=timeout,
+                        timeout=config_data['timeout'],
                         cache_path=dask_cache_path)
 
     get_changepoints_dask(pca_components=pca_components, pca_scores=pca_file_scores,
                           h5s=h5s, yamls=yamls, changepoint_params=changepoint_params,
-                          save_file=save_file, chunk_size=chunk_size,
-                          fps=fps, client=client, missing_data=missing_data,
+                          save_file=save_file, chunk_size=config_data['chunk_size'],
+                          fps=config_data['fps'], client=client, missing_data=missing_data,
                           mask_params=mask_params)
 
     if cluster is not None:
