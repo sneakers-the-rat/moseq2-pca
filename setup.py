ecb50c2f44357f71f5fd0d4782c053d0335532a2
Ayman Zeine <a.zeine.96@gmail.com>
2020-04-29 12:17:13 -0400

    CI: dependency version fix

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=['h5py==2.10.0', 'matplotlib', 'scipy==1.3.3',
                       'tqdm==4.40.0', 'numpy==1.17.4', 'joblib==0.14.0',
                       'click==7.0', 'ruamel.yaml==0.16.5', 'numpy==1.17.4',
-                      'dask==2.8.1', 'distributed==2.8.1', 'chest==0.2.3', 'seaborn==0.9.0', 'dask-jobqueue==0.7.0',
+                      'dask>=2.8.1', 'distributed==2.8.1', 'chest==0.2.3', 'seaborn==0.9.0', 'dask-jobqueue==0.7.0',
                       'scikit-image==0.16.2', 'bokeh==1.4.0', 'psutil==5.6.7'],
     python_requires='>=3.6',
     entry_points={'console_scripts': ['moseq2-pca = moseq2_pca.cli:cli']}
