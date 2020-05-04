fa26b822023f0c929deff007b854c6d825b24842
Ayman Zeine <a.zeine.96@gmail.com>
2020-05-04 10:26:47 -0400

    locked matplotlib version number

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     version='0.2.2',
     platforms=['mac', 'unix'],
     packages=find_packages(),
-    install_requires=['h5py==2.10.0', 'matplotlib', 'scipy==1.3.3',
+    install_requires=['h5py==2.10.0', 'matplotlib==3.1.2', 'scipy==1.3.3',
                       'tqdm==4.40.0', 'numpy==1.17.4', 'joblib==0.14.0',
                       'click==7.0', 'ruamel.yaml==0.16.5', 'numpy==1.17.4',
                       'dask==2.11.0', 'distributed==2.11.0', 'chest==0.2.3', 'seaborn==0.9.0', 'dask-jobqueue==0.7.0',
