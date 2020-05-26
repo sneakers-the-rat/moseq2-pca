6f3328bf92126f5d0438ded750c61243281d40ba
Ayman Zeine <a.zeine.96@gmail.com>
2020-05-26 18:58:42 -0400

    Removed incorrect file loads

--- moseq2_pca/pca/util.py
+++ moseq2_pca/pca/util.py
@@ -498,7 +498,7 @@ def get_changepoints_dask(changepoint_params, pca_components, h5s, yamls,
 
         with h5py.File(h5, 'r') as f:
 
-            dset = h5py.File(h5, mode='r')[h5_path][()]
+            dset = h5py.File(h5, mode='r')[h5_path]
             frames = da.from_array(dset, chunks=(chunk_size, -1, -1)).astype('float32')
 
             timestamps = get_timestamps(f, frames, fps)
@@ -506,7 +506,7 @@ def get_changepoints_dask(changepoint_params, pca_components, h5s, yamls,
         if missing_data and pca_scores is None:
             raise RuntimeError("Need to compute PC scores to impute missing data")
         elif missing_data:
-            mask_dset = h5py.File(h5, mode='r')[h5_mask_path][()]
+            mask_dset = h5py.File(h5, mode='r')[h5_mask_path]
             mask = da.from_array(mask_dset, chunks=frames.chunks)
             mask = da.logical_and(mask < mask_params['mask_threshold'],
                                   frames > mask_params['mask_height_threshold'])
@@ -514,8 +514,8 @@ def get_changepoints_dask(changepoint_params, pca_components, h5s, yamls,
             mask = mask.reshape(-1, frames.shape[1] * frames.shape[2])
 
             with h5py.File(pca_scores, 'r') as f:
-                scores = f['scores/{}'.format(uuid)][()]
-                scores_idx = f['scores_idx/{}'.format(uuid)][()]
+                scores = f['scores/{}'.format(uuid)]
+                scores_idx = f['scores_idx/{}'.format(uuid)]
                 scores = scores[~np.isnan(scores_idx), :]
 
             if np.sum(frames.chunks[0]) != scores.shape[0]:
