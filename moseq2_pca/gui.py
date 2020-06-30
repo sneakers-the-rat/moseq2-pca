f4f9a4ded1d6c4d53aeb82093944b8a2a8b92080
Winthrop Gillis <win.gillis@gmail.com>
2020-06-30 16:53:47 -0400

    updated code to override default config settings
    new way is easier to read and simpler

--- moseq2_pca/gui.py
+++ moseq2_pca/gui.py
@@ -3,6 +3,7 @@ import ruamel.yaml as yaml
 from .cli import train_pca, apply_pca, compute_changepoints
 from moseq2_pca.helpers.wrappers import train_pca_wrapper, apply_pca_wrapper, compute_changepoints_wrapper
 
+
 def train_pca_command(input_dir, config_file, output_dir, output_file):
     '''
     Train PCA through Jupyter notebook, and updates config file.
@@ -26,12 +27,10 @@ def train_pca_command(input_dir, config_file, output_dir, output_file):
         config_data = yaml.safe_load(f)
 
     # Get default CLI params
-    objs = train_pca.params
+    default_params = {tmp.name: tmp.default for tmp in train_pca.params if not tmp.required}
 
-    params = {tmp.name: tmp.default for tmp in objs if not tmp.required}
-    for k, v in params.items():
-        if k not in config_data.keys():
-            config_data[k] = v
+    # merge default params with those in config
+    config_data = {**default_params, **config_data}
 
     with open(config_file, 'w') as f:
         yaml.safe_dump(config_data, f)
@@ -40,6 +39,7 @@ def train_pca_command(input_dir, config_file, output_dir, output_file):
 
     with open(config_file, 'w') as f:
         yaml.safe_dump(config_data, f)
+
 
 def apply_pca_command(input_dir, index_file, config_file, output_dir, output_file):
     '''
@@ -58,7 +58,6 @@ def apply_pca_command(input_dir, index_file, config_file, output_dir, output_fil
     (str): success string.
     '''
 
-    # find directories with .dat files that either have incomplete or no extractions
     # TODO: additional post-processing, intelligent mapping of metadata to group names, make sure
     # moseq2-model processes these files correctly
 
@@ -66,12 +65,10 @@ def apply_pca_command(input_dir, index_file, config_file, output_dir, output_fil
         config_data = yaml.safe_load(f)
 
     # Get default CLI params
-    objs = apply_pca.params
+    default_params = {tmp.name: tmp.default for tmp in apply_pca.params if not tmp.required}
 
-    params = {tmp.name: tmp.default for tmp in objs if not tmp.required}
-    for k, v in params.items():
-        if k not in config_data.keys():
-            config_data[k] = v
+    # merge default params with those in config
+    config_data = {**default_params, **config_data}
 
     config_data = apply_pca_wrapper(input_dir, config_data, output_dir, output_file, gui=True)
 
@@ -113,17 +110,15 @@ def compute_changepoints_command(input_dir, config_file, output_dir, output_file
         config_data = yaml.safe_load(f)
 
     # Get default CLI params
-    objs = compute_changepoints.params
+    default_params = {tmp.name: tmp.default for tmp in compute_changepoints.params
+                      if not tmp.required}
 
-    params = {tmp.name: tmp.default for tmp in objs if not tmp.required}
-    for k, v in params.items():
-        if k not in config_data.keys():
-            config_data[k] = v
+    # merge default params with those in config
+    config_data = {**default_params, **config_data}
 
     config_data = compute_changepoints_wrapper(input_dir, config_data, output_dir, output_file, gui=True)
 
     with open(config_file, 'w') as f:
         yaml.safe_dump(config_data, f)
-
 
     return 'Model-free syllable changepoints have been successfully computed.'
