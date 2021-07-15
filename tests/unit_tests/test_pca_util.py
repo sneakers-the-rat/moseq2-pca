ab12322564c0eb2f6ea9c2aae3c29c35f8b2bb88
Ayman Zeine <a.zeine.96@gmail.com>
2021-07-15 15:52:32 -0400

    fix: reverted test exception to `ValueError` for printing `.keys()`

--- tests/unit_tests/test_pca_util.py
+++ tests/unit_tests/test_pca_util.py
@@ -93,8 +93,8 @@ class TestPCAUtils(TestCase):
             try:
                 print(fp['frames'].keys()) # line that's meant to raise a ValueError
                 assert False, 'h5 File pointer is still open' # added assertion here to fail test in case file is still open
-            except TypeError as e:
-                assert isinstance(e, TypeError)
+            except ValueError as e:
+                assert isinstance(e, ValueError)
 
     def test_apply_pca_local(self):
 
