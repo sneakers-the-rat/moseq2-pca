f9ed8bbf2f1d34c25f0120ecffa044f2f899ee8d
Sherry <sherrylin42@gmail.com>
2021-12-09 17:15:29 -0500

    chore: update test script

--- scripts/download_test_dataset.sh
+++ scripts/download_test_dataset.sh
@@ -1,2 +1,2 @@
-curl -L -o data.zip https://www.dropbox.com/sh/b0tpybq283a85ow/AABinnaFTGmUUcYOQ3GtwpRBa?dl=0 && unzip data.zip -d data
+curl -L -o data.zip https://www.dropbox.com/sh/ki8vu9iwapvsk5a/AADipJ4LpTDRQ76KlcxMbgoAa?dl=0 && unzip data.zip -d data
 rm data.zip
